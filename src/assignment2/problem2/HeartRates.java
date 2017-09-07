70d516950aa595d7583eb54f676ab6e6577bf12c
Tulsi <neeman.drn@gmail.com>
2017-09-06 23:32:44 -0500

    update

--- src/assignment2/problem2/HeartRates.java
+++ src/assignment2/problem2/HeartRates.java
@@ -1,8 +1,7 @@
 package assignment2.problem2;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Scanner;
+import java.time.Period;
 
 /**
  * Assignment 2 Problem 2
@@ -10,31 +9,75 @@ import java.util.Scanner;
  *
  */
 public class HeartRates {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-M-d");
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.print("Enter first name: ");
-		String fName = scanner.nextLine();
-		
-		System.out.print("Enter last name: ");
-		String lName = scanner.nextLine();
-		
-		System.out.print("Enter dob in format yyyy-M-d");
-		String dob = scanner.nextLine();
-		
-		LocalDate lDob = LocalDate.parse(dob, formatter);
-		Person person = new Person(fName, lName, lDob);
-		
-		System.out.println("\na. person's age: " + person.getAge());
-		System.out.println("b. person's max heart rate: " + person.getMaxmimumHeartRate());
-		System.out.println("c. person's target heart range: " + person.getheartRateRange());
-		System.out.println("d. person's toString() data: " + person.toString());
-		
-		scanner.close();
+	private String fname, lName;
+	private LocalDate dob;
+	
+	private final int rhr = 70;
+	private final int maxHR = 220;
+	private final float lowerBoundry = 0.5f;
+	private final float upperBoundry = 0.85f;
+	
+	public HeartRates(String fname, String lName, LocalDate dob) {
+		super();
+		this.fname = fname;
+		this.lName = lName;
+		this.dob = dob;
 	}
 
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getlName() {
+		return lName;
+	}
+
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+
+	public LocalDate getDob() {
+		return dob;
+	}
+
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+	
+	// calculate age
+	public int getAge() {
+		LocalDate today = LocalDate.now();
+        return Period.between(this.dob, today).getYears();
+	}
+	
+	// calculate maximum heart rate
+	public Integer getMaxmimumHeartRate() {
+		return maxHR - this.getAge();
+	}
+	
+	// calculate average heart rate
+	public Integer getAverageHeartRate() {
+		return this.getMaxmimumHeartRate() - rhr;
+	}
+	
+	// calculate range of heart beat
+	public String getheartRateRange() {
+		float lbthr = this.getAverageHeartRate()*lowerBoundry + rhr;
+		float ubthr = this.getAverageHeartRate()*upperBoundry + rhr;
+		
+		return lbthr + " - " + ubthr;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [fname=" + fname + ", lName=" + lName + ", dob=" + dob + ", getAge()=" + getAge()
+				+ ", getMaxmimumHeartRate()=" + getMaxmimumHeartRate() + ", getheartRateRange()=" + getheartRateRange() + "]";
+	}
+	
+	
+	
 }
--- src/assignment2/problem3/MyDate.java
+++ src/assignment2/problem3/MyDate.java
@@ -1,6 +1,9 @@
 package assignment2.problem3;
 
-import java.time.LocalTime;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 /**
  * Assignment 2 Problem 3
@@ -8,13 +11,190 @@ import java.time.LocalTime;
  *
  */
 public class MyDate {
+	
+	private static DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+	private static DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("MMMM dd, yyyy");
+	private static DateTimeFormatter formatter3 = DateTimeFormatter.ofPattern("D yyyy");
+	
+	public MyDate(int month, int day, int year) {
+		System.out.println("\tMonth: " + month + "\t Day: " + day + "\t Year:" + year);
+		LocalDate date = LocalDate.of(year, month, day);
+		System.out.println("\t" + date.format(formatter1));
+		System.out.println("\t" + date.format(formatter2));
+		System.out.println("\t" + date.format(formatter3) + "\n");
+	}
+	
+	public MyDate(String month, int day, int year) {
+		System.out.println("\tMonth: " + month + "\t Day: " + day + "\t Year:" + year);
+		LocalDate date = LocalDate.of(year, Month.valueOf(month), day);
+		System.out.println("\t" + date.format(formatter1));
+		System.out.println("\t" + date.format(formatter2));
+		System.out.println("\t" + date.format(formatter3) + "\n");
+	}
+	
+	public MyDate(int day, int year) {
+		System.out.println("\tDay: "+ day + "\t Year:" + year);
+		LocalDate date = LocalDate.ofYearDay(year, day);
+		System.out.println("\t" + date.format(formatter1));
+		System.out.println("\t" + date.format(formatter2));
+		System.out.println("\t" + date.format(formatter3) + "\n");
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		final String DATE_PATTERN1 = "MM/dd/yyyy";
 		
-		LocalTime localTime = LocalTime.now();
-		System.out.println(localTime);
+		Scanner sc = new Scanner(System.in);
+		
+		LocalDate date = LocalDate.now();
+		int val;
+		int month, day, year;
+		String mon, out;
+		
+		System.out.println("a. Today date in multiple formats:-");
+		System.out.println("\t" + date.format(formatter1));
+		System.out.println("\t" + date.format(formatter2));
+		System.out.println("\t" + date.format(formatter3) + "\n");
+		
+		System.out.println("b. overloaded constructors:-");
+		new MyDate(date.getMonthValue(), date.getDayOfMonth(), date.getYear());
+		new MyDate(date.getMonth().name(), date.getDayOfMonth(), date.getYear());
+		new MyDate(date.getDayOfYear(), date.getYear());
+		
+		int n = 0;
+		while(n<1) {
+			
+			System.out.println("\nEnter 1 for format: MM/DD/YYYY\r\n" + 
+					"Enter 2 for format: Month DD, YYYY\r\n" + 
+					"Enter 3 for format: DDD YYYY\r\n" + 
+					"Enter 4 to exit\n");
+			val = sc.nextInt();
+			
+			sc.nextLine();
+			
+			while(true) {
+				if(val < 1  || val > 4) {
+					System.out.println("Invalid input. Please input it again");
+					val = sc.nextInt();
+					
+					if(val > 0 && val < 5 ) {
+						break;
+					}
+				} else {
+					break;
+				}
+			}
+			
+			switch (val) {
+				case 1:
+
+					System.out.println("Enter Month: ");
+					month = sc.nextInt();
+					
+					sc.nextLine();
+					
+					System.out.println("Enter Day: ");
+					day = sc.nextInt();
+					
+					sc.nextLine();
+					
+					System.out.println("Enter Year: ");
+					year = sc.nextInt();
+					
+					sc.nextLine();
+					
+					date = LocalDate.of(year, month, day);
+					System.out.println("\tMM/dd/yyyy: " + date.format(formatter1));
+					System.out.println("\tMMMM dd,yyyy:" + date.format(formatter2));
+					System.out.println("\tDDD yyyy: " + date.format(formatter3) + "\n");
+					
+					System.out.println("Do you want to Continue (y/n):");
+					out = sc.nextLine();
+					
+					if(out.equals("n")) {
+						System.out.println("Terminated");
+						n=1;
+					}else {
+						n=0;
+					}
+					
+					break;
+					
+				case 2:
+					
+					System.out.println("Enter Month: ");
+					mon = sc.nextLine();
+					
+					//sc.nextInt();
+					
+					System.out.println("Enter Day: ");
+					day = sc.nextInt();
+					
+					sc.nextLine();
+					
+					System.out.println("Enter Year: ");
+					year = sc.nextInt();
+					
+					sc.nextLine();
+					
+					date = LocalDate.of(year, Month.valueOf(mon.toUpperCase()), day);
+					System.out.println("\tMM/dd/yyyy: " + date.format(formatter1));
+					System.out.println("\tMMMM dd,yyyy:" + date.format(formatter2));
+					System.out.println("\tDDD yyyy: " + date.format(formatter3) + "\n");
+					
+					System.out.println("Do you want to Continue (y/n):");
+					out = sc.nextLine();
+					
+					if(out.equals("n")) {
+						System.out.println("Terminated");
+						n=1;
+					}else {
+						n=0;
+					}
+					
+					break;
+					
+				case 3:
+					
+					System.out.println("Enter Days of Year: ");
+					day = sc.nextInt();
+					
+					sc.nextLine();
+					
+					System.out.println("Enter Year: ");
+					year = sc.nextInt();
+					
+					sc.nextLine();
+					
+					date = LocalDate.ofYearDay(year, day);
+					System.out.println("\tMM/dd/yyyy: " + date.format(formatter1));
+					System.out.println("\tMMMM dd,yyyy:" + date.format(formatter2));
+					System.out.println("\tDDD yyyy: " + date.format(formatter3) + "\n");
+					
+					System.out.println("Do you want to Continue (y/n):");
+					out = sc.nextLine();
+					
+					if(out.equals("n")) {
+						System.out.println("Terminated");
+						n=1;
+					}else {
+						n=0;
+					}
+					break;
+					
+				case 4:
+					System.out.println("Terminated");
+					n=1;
+					
+					break;
+				default:
+					System.out.println("Terminated");
+					n=1;
+					
+					break;
+			}
+		}
+		
+		sc.close();
+		
 	}
 
 }
