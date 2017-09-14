eec4d710e2085d7ad8d70930f7a085837a4d6227
unknown <985955@CS.MUM.EDU>
2017-09-14 14:41:40 -0500

    asignment 6 added

---
+++ src/assignment6/problem2/Main.java
@@ -0,0 +1,22 @@
+package assignment6.problem2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MyTable t = new MyTable();
+		t.add('a', "Andrew");
+		t.add('b',"Billy");
+		t.add('w',"Willie");
+		
+		
+		t.add('a', "Annie");
+		
+		System.out.println(t);
+		
+		String s = t.get('w');
+		System.out.println(s);
+	}
+
+}
